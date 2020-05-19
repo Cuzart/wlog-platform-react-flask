ac277f8b8f44701bc972c4ffa0f5ccc09f2f7998
jonas <jl121@hdm-stuttgart.de>
2020-05-19 19:25:00 +0200

    added user model for DB

--- api/app.py
+++ api/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, url_for
 from markupsafe import escape
-import db.mariadb as mariadb
-import db.user as user
+import json
+
+from db.user import User
+#import db.user as user
 
 app = Flask(__name__)
-db = mariadb.connect()
+#db = mariadb.connect()
 
 
 @app.route('/')
@@ -17,19 +19,15 @@ def hello(name):
     return "moin wie gehts dir " + name
 
 
-@app.route('/dbtest/get')
-def dbGet():
-
-    mycursor = db.cursor()
-    mycursor.execute("SELECT * FROM users")
-    myresult = mycursor.fetchall()
-    return str(myresult)
+@app.route('/dbtest/get/<id>')
+def dbGet(id):
+    return json.dumps(User.get(id)[1])
 
 
 @app.route('/dbtest/add')
 def dbAdd():
-    customer = user.user("peter224", "peter.schwarz@gmail.com",
-                         "324323dd", "Peter", "Schwarz")
-    customer.description = "bliblablub"
-    customer.profilpicture = "pp_peter224"
-    return str(customer.insert())
+    user = User("Walter32", "walter.maier@gmail.com",
+                "passwort22", "Walter", "Meier")
+    #user.description = "bliblablub"
+    #user.profilpicture = "pp_peter224"
+    return str(user.insert())
