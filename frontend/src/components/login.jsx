e9f941e18c12ded43a5f3ca3f70bf15381d4df24
ch148 <ch148@hdm-stuttgart.de>
2020-05-21 09:42:21 +0200

    login/ register component

--- frontend/src/components/login.jsx
+++ frontend/src/components/login.jsx
@@ -1,27 +1,23 @@
 import React from "react";
 
 export class Login extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
-      <div className="base-container" ref={this.props.containerRef}>
-        <div className="header">Login</div>
-        <div className="content">
-          <div className="form">
-            <div className="form-group">
+      <div>
+        <div>Login</div>
+        <div>
+          <div>
+            <div>
               <label htmlFor="username">Username</label>
               <input type="text" name="username" placeholder="username" />
             </div>
-            <div className="form-group">
+            <div>
               <label htmlFor="password">Password</label>
               <input type="password" name="password" placeholder="password" />
             </div>
           </div>
         </div>
-        <div className="footer">
+        <div>
           <button type="button" className="btn">
             Login
           </button>
--- frontend/src/components/pages/Registry.jsx
+++ frontend/src/components/pages/Registry.jsx
@@ -1,11 +1,90 @@
 import React from "react";
+//import axios from "axios";
 
-function Registry() {
-  return (
-    <React.Fragment>
-      <h1>Registry Page</h1>
-    </React.Fragment>
-  );
+class Registry extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      email: "",
+      password: "",
+      name: "",
+      surname: "",
+      changed: false,
+    };
+  }
+
+  // sends state object to REST API
+  handleSubmit = (event) => {
+    event.preventDefault();
+    let user = this.state;
+    console.log(user);
+    //axios.post("api:5000/registry", user).then((res) => console.log(res));
+  };
+
+  // updates state object when form is changed
+  handleChange = (event) => {
+    let nam = event.target.name;
+    let val = event.target.value;
+    this.setState({ [nam]: val });
+    this.setState({ changed: true });
+  };
+
+  // register form
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            placeholder="Username"
+            name="username"
+            onChange={this.handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">E-Mail</label>
+          <input
+            type="text"
+            placeholder="E-Mail"
+            name="email"
+            onChange={this.handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="text"
+            placeholder="Password"
+            name="password"
+            onChange={this.handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            onChange={this.handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="surname">Surname</label>
+          <input
+            type="text"
+            placeholder="Surname"
+            name="surname"
+            onChange={this.handleChange}
+          />
+        </div>
+        <div>
+          <button type="submit"> Register</button>
+        </div>
+      </form>
+    );
+  }
 }
 
 export default Registry;
