dbd08b5ce808eb35bd19482a935c8c9f973eb143
jonas <jl121@hdm-stuttgart.de>
2020-05-27 21:53:09 +0200

    added simple image upload

--- backend/api/routes.py
+++ backend/api/routes.py
@@ -1,7 +1,13 @@
 from api import app
-from flask import url_for, request, render_template
+from flask import url_for, request, redirect, render_template, send_from_directory
+import os
 import json
+from werkzeug.utils import secure_filename
+# own modules
 from api.db.user import User
+from api.helper import allowed_file
+
+ALLOWED_IMAGE_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 
 @app.route('/')
@@ -10,12 +16,9 @@ def index():
 
 
 # development for testing sites
-@app.route('/<string:name>Test')
+@app.route('/test')
 def hello(name):
-    if name == "jonas" or name == "agil":
-        return render_template('{}Test.html'.format(name), data="Hallo")
-    else:
-        return name
+    return "hallo"
 
 
 @app.route('/profil/<int:id>')
@@ -39,3 +42,28 @@ def register():
     user = User(userData)
     user.save()
     return "successfully registerd"
+
+
+@app.route('/upload', methods=["GET", "POST"])
+def upload():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            return 'No selected file'
+        if file and allowed_file(file.filename, ALLOWED_IMAGE_EXTENSIONS):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join("/usr/src/app/images", filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return render_template("fileUpload.html")
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory("/usr/src/app/images",
+                               filename)
