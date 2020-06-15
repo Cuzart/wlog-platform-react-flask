f9fe44ba7fd06d3d8c152c99f01a89f91a608047
ch148 <ch148@hdm-stuttgart.de>
2020-06-15 09:06:37 +0200

    jQuery und die create Form für Trips/Blogs

--- frontend/src/components/TripForm.jsx
+++ frontend/src/components/TripForm.jsx
@@ -1,51 +1,20 @@
 import React, { Component } from "react";
-import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import FormGroup from "react-bootstrap/FormGroup";
 import axios from "axios";
 
+
 class TripForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      title: "",
-      thumbnail: null,
-      country: "",
-      description: "",
-    };
-  }
-
-  // updates thumbnail in state when new file is selected
-  handleFileSelect = (event) => {
-    this.setState({
-      thumbnail: event.target.files[0],
-    });
-    // document
-    //   .getElementById("frame")
-    //   .setAttribute("src", URL.createObjectURL(event.target.files[0]));
-  };
-
-  // File Upload
+  // File Upload as Form Data to DB
   handleFileUpload = (event) => {
     const fd = new FormData();
     fd.append("image", this.state.thumbnail, this.state.thumbnail.name);
+    console.log(fd);
     axios.post("url", fd).then((res) => {
       console.log(res);
     });
-  };
-
-  // Updates state when form is changed
-  handleChange = (event) => {
-    let nam = event.target.name;
-    let val = event.target.value;
-    this.setState({ [nam]: val });
-  };
-  //Submitting the Form
-  handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(this.state.thumbnail);
   };
 
   render() {
@@ -53,7 +22,7 @@ class TripForm extends Component {
       <div>
         <div className="container">
           <h1 style={headerStyles}> Create an individual trip </h1> <br />
-          <Form onSubmit={this.handleSubmit}>
+          <Form>
             <FormGroup as={Row}>
               <Form.Label column sm={1}>
                 Title
@@ -62,7 +31,7 @@ class TripForm extends Component {
                 <Form.Control
                   name="title"
                   type="text"
-                  onChange={this.handleChange}
+                  onChange={this.props.handleChange}
                 />
               </Col>
             </FormGroup>
@@ -72,9 +41,10 @@ class TripForm extends Component {
               </Form.Label>
               <Col sm={5}>
                 <Form.File
+                  id="formfile"
                   accept="image/png, image/jpeg"
-                  onChange={this.handleFileSelect}
-                  label="4:3 format"
+                  onChange={this.props.handleFileSelect}
+                  label={this.props.fileFormLabel}
                   custom
                 />
               </Col>
@@ -86,7 +56,7 @@ class TripForm extends Component {
               <Col sm={5}>
                 <Form.Control
                   name="country"
-                  onChange={this.handleChange}
+                  onChange={this.props.handleChange}
                   type="text"
                 />
               </Col>
@@ -99,41 +69,26 @@ class TripForm extends Component {
                 <Form.Control
                   as="textarea"
                   name="description"
-                  onChange={this.handleChange}
+                  onChange={this.props.handleChange}
                   rows="3"
                 />
               </Col>
             </FormGroup>
-            <Button variant="dark" type="submit">
-              Test Image Upload
-            </Button>
-            <hr />
+            <hr style={hrStyle} />
           </Form>
         </div>
-        {/* <Col>
-          <img
-            style={previewStyle}
-            id="frame"
-            alt="thumbnail-preview"
-            src=""
-            width="400px"
-            height="300px"
-          />
-        </Col> */}
       </div>
     );
   }
 }
-
 const headerStyles = {
   fontFamily: "Libre Baskerville , serif",
   margin: "35px 0px",
 };
-// const previewStyle = {
-//   visibility: "hidden",
-//   padding: "50px",
-//   left: "60%",
-//   marginTop: "340px",
-// };
+
+const hrStyle = {
+  margin: "60px 0px",
+};
+
 
 export default TripForm;
--- frontend/src/components/layout/SaveChangesModal.jsx
+++ frontend/src/components/layout/SaveChangesModal.jsx
@@ -3,48 +3,36 @@ import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 
 class SaveChangesModal extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      open: false,
-      data: null,
-    };
-  }
-  static getDerivedStateFromProps = (props, state) => {
-    return { open: props.show };
-  };
-  test = (props) => {
-    this.setState({ open: props });
-    this.setState({ data: this.props.data });
-    alert(this.state.data);
-  };
-
   render() {
     return (
-      <Modal
-        {...this.props}
-        open={this.props.open}
-        size="md"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-      >
-        <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">
-            {this.props.heading}
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <p>{this.props.content}</p>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={this.props.onHide}>
-            Back
+      <div>
+        <Modal
+          {...this.props}
+          open={this.props.open}
+          size="md"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+        >
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">
+              {this.props.heading}
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p>{this.props.content}</p>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={this.props.onHide}>
+              Back
           </Button>
-          <Button variant="dark" onClick={this.test}>
-            Save changes
+            <Button variant="dark" onClick={this.props.onSubmit}>
+              Save changes
           </Button>
-        </Modal.Footer>
-      </Modal>
+          </Modal.Footer>
+        </Modal>
+
+      </div>
+
     );
   }
 }
