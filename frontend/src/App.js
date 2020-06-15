372e4b5a5efe85dc885c7015f4a3c86f4b92d8bf
ch148 <ch148@hdm-stuttgart.de>
2020-06-15 20:07:27 +0200

    AddPost Form /add with Location Field

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Header from "./components/layout/Header";
 import Register from "./components/pages/Register";
 import Home from "./components/pages/Home";
 import CreatePost from "./components/pages/CreatePost";
+import AddPost from "./components/pages/AddPost";
 
 class App extends Component {
   render() {
@@ -17,6 +18,7 @@ class App extends Component {
             <Route exact path="/" component={Home} />
             <Route exact path="/register" component={Register} />
             <Route exact path="/create" component={CreatePost} />
+            <Route exact path="/add" component={AddPost} />
           </Switch>
         </div>
       </Router>
