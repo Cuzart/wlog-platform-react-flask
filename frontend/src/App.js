41f07236464a48d1edffd103ed88555f911fc8d7
ch148 <ch148@hdm-stuttgart.de>
2020-06-20 09:21:16 +0200

    Leaflet Map and Homepage fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import Register from "./components/pages/Register";
 import Home from "./components/pages/Home";
 import CreatePost from "./components/pages/CreatePost";
 import AddPost from "./components/pages/AddPost";
+import LeafletMap from "./components/LeafletMap"
 
 class App extends Component {
   render() {
@@ -19,6 +20,7 @@ class App extends Component {
             <Route exact path="/register" component={Register} />
             <Route exact path="/create" component={CreatePost} />
             <Route exact path="/add" component={AddPost} />
+            <Route exact path="/map" component={LeafletMap} />
           </Switch>
         </div>
       </Router>
--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -1,17 +1,62 @@
 import React, { Component } from "react";
 import "../../App.css";
 import Login from "../Login";
+import CreatePost from "./CreatePost"
 
 class Home extends Component {
   render() {
     return (
       <div>
         <div className="home-bg">
-          <Login />;
+          <div className="container">
+            <div style={loginForm}>
+              <Login />
+            </div>
+            <div style={textStyle}>
+              A home for <br /> your <span style={{ color:"#9EB091"}}> memories </span>
+            </div>
+        
+          </div>
+         
         </div>
+        <div style={barStyle}>
+          <img src="/images/bar.svg" />
+        </div>
+        <div className="App" height="500px">
+           
+        </div>
+        
       </div>
     );
   }
+}
+
+const loginForm = {
+  position: "absolute",
+  height: "auto",
+  width: "340px",
+  top: "43%",
+  background: "#9EB091",
+  borderRadius: "50px",
+  textAlign: "left",
+};
+
+const textStyle = {
+  position: "absolute",
+  textAlign: "right",
+  top: "45%",
+  left: "50%",
+  fontFamily: "Libre Baskerville , serif",
+  padding: "20px",
+  fontSize: "50pt",
+  color:"white",
+  textShadow: "2px 2px #000000c0",
+}
+
+const barStyle = {
+  position: "grid",
+  marginTop: "-300px",
+  bottom: "-200px",
 }
 
 export default Home;
